7031d6b5c3a71d7a3f7ab0817a4a596d2d2e1c34
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-14 18:32:18 +0000

    Deploy kewalkothari/govocal4local to github.com/kewalkothari/govocal4local.git:gh-pages

---
+++ precache-manifest.4dce64c1d0ab30f8d83b93888cbcad43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36be35b8af7dc5603448afc3f6c65aa0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15d79869bf22fa988628",
+    "url": "/static/css/main.e28e174a.chunk.css"
+  },
+  {
+    "revision": "9b83a8a59beb31e8f34d",
+    "url": "/static/js/2.a13c5f31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a13c5f31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15d79869bf22fa988628",
+    "url": "/static/js/main.d673c8c2.chunk.js"
+  },
+  {
+    "revision": "8ddf0c8ef00950fc7488",
+    "url": "/static/js/runtime-main.19ad6934.js"
+  }
+]);
