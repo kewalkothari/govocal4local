0002e70ede1e0f91b733113db8ae11effa775f86
Kewal Kothari <kotharikewal7@gmail.com>
2020-05-14 12:04:29 +0530

    Add coming soon page.

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Loader.scss';
 
 const loader = 
-<div className="wrapper">
+<div>
 <div className="center title">#GoVocal4Local</div>
 <div className="demo-3 center">
     <ul className="spinner">
--- src/static-pages/coming-soon/ComingSoon.jsx
+++ src/static-pages/coming-soon/ComingSoon.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './ComingSoon.scss';
+import Loader from '../../components/Loader/Loader';
 
 const comingSoonPage =
  <div className="wrapper">
+    <div className="heading">Coming Soon!</div>
+    <Loader/>
+    <div className="caption">Discover the power within.</div>
 </div>;
 
 export default comingSoonPage;
